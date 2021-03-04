92cc16efd879612cc68c5528315b4b1b82dff9fe
Júnio Teixeira <junio-82@hotmail.com>
2021-03-04 14:42:23 -0300

    Fixed tests

--- iugu.net.IntegratedTests/GlobalSetup.cs
+++ iugu.net.IntegratedTests/GlobalSetup.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 
 namespace iugu.net.IntegratedTests
 {
@@ -8,9 +12,16 @@ namespace iugu.net.IntegratedTests
         [OneTimeSetUp]
         public void IuguClientConfig()
         {
+            string message = "{\"Message\": { \"Errors\": [\"Teste\", \"GG\" ]}}";
+            string message2 = "{\"Message\": \"Ola\"}";
+            var json = JObject.Parse(message);
+            var json2 = JObject.Parse(message2);
+
+            var test = json["Message"] is IDictionary<string, JToken>;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; 
             IuguClient.Init(new IuguClientProperties()
             {
-                ApiKey = "74c265aedbfaea379bc0148fae9b5526"
+                ApiKey = "0efa33557aa042fd13d9275ce310f5b8"
             });
         }
     }
